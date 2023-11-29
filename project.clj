56a9628c4cc309b3217998bc03072d08b46cc488
sandmark <sandmark.m@gmail.com>
2023-11-29 23:16:27 +0900

    Convert from leiningen to deps.edn

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject clojure-ini "0.0.2"
-  :description "Read .ini files into Clojure maps"
-  :license "Eclipse Public License 1.0"
-  :url "http://github.com/jonase/clojure-ini"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :global-vars {*warn-on-reflection* true})
